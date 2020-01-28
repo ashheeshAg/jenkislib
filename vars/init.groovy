ea697c50b036f9c4acfcb38b9e375146aec6acdb
Tarun17 <31914620+Tarun17@users.noreply.github.com>
2020-01-28 19:05:34 +0530

    Update init.groovy

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,5 +1,8 @@
-def init() {
-    sh  """
-                  terraform init -input=false
-  """
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
   }
