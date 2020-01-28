501798190fb475a1cc616408903a7b15f4aa05ee
Ashish Agrawal <12447039+ashheeshAg@users.noreply.github.com>
2020-01-28 19:58:10 +0530

    Update apply.groovy

--- vars/apply.groovy
+++ vars/apply.groovy
@@ -1,7 +1,12 @@
-def call(Map stageParams) {
- 
-   sh  """  
-                  cd terraform-plans/create
-                  terraform apply -input=false -auto-approve "tfplan"
-                  """
-  }
+#!/usr/bin/env groovy
+
+def call(String version, String appName, String env, String terraformDir=appName) {
+
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '1e254ac4-b4dc-4776-934a-68648fc697aa', url: '']]])
+    writeFile file: "${terraformDir}/${env}/terraform.tfvars", text: """
+    """
+    withCredentials([usernamePassword(credentialsId: '1e254ac4-b4dc-4776-934a-68648fc697aa', passwordVariable: 'AZURE_SECRET_ACCESS_KEY', usernameVariable: 'AZURE_ACCESS_KEY_ID')]) {
+        sh "terraform apply -e ${terraformDir}/${env}"
+    }
+}
+
